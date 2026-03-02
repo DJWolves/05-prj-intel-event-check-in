8b83f11c3867c8c9c3e47094ee71a9ab87537eb3
DJWolves <53882884+DJWolves@users.noreply.github.com>
2026-03-02 01:33:56 +0000

    greeting and progressbar update

--- script.js
+++ script.js
@@ -1,56 +1,44 @@
-
 //get all elements
 const form = document.getElementById("checkInForm");
 const attendee = document.getElementById("attendeeName");
 const team = document.getElementById("teamSelect");
-
-
+const progressBar = document.getElementById("progressBar");
+const attendeeCount = document.getElementById("attendeeCount");
+const greeting = document.getElementById("greeting");
 
 ///attendance number
 
 let count = 0;
 const maxCount = 50;
 
-
-
 //form submission stuff
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-
-//form values
+  //form values
   const name = attendee.value;
   const selectedTeam = team.value;
   const teamName = team.selectedOptions[0].text;
 
   console.log(name, teamName);
 
-
-
   //increment count
-count++
-console.log("total checkins", count);
+  count++;
+  console.log("total checkins", count);
 
+  //progressbar updater
+  const percentage = Math.round((count / maxCount) * 100) + "%";
+  console.log("Progress:", percentage);
+  progressBar.style.width = percentage;
+  attendeeCount.textContent = count;
 
+  //team counter
+  const teamCounter = document.getElementById(selectedTeam + "Count");
+  teamCounter.textContent = parseInt(teamCounter.textContent) + 1;
 
-//progressbar updater
-const percentage = Math.round((count / maxCount) * 100) + "%";
-console.log('Progress:', percentage);
+  const message = `Welcome, ${name} from ${teamName}`;
+  console.log(message);
+  greeting.textContent = message;
 
-
-//team counter
-const teamCounter = document.getElementById(selectedTeam + "Count");
- teamCounter.textContent = parseInt(teamCounter.textContent) +1;
-
-
-const message = `Welcome, ${name} from ${teamName}`;
-console.log(message);
-
-
-form.reset();
+  form.reset();
 });
-
-
-
-
-
